d2517dfe72cf728ddd5c1558e64f9aa8bc4ecadc
hossein <h@h.com>
2025-09-05 01:29:30 +0330

    Change DEBUG setting to False and restrict ALLOWED_HOSTS to specific domain.

--- NYT/settings.py
+++ NYT/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_+$*6*6)omtrh@y^vobns2y(w7cqnw=7$j&tvm3&w0%5xfmgr4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['db.shraif.ir']
 CSRF_TRUSTED_ORIGINS = ['https://db.shraif.ir']
 # Application definition
 
